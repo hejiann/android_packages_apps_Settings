dbce7372fec26a4644ddee7339bd3f9284025bb8
tongchao <tongchao@shendu.com>
2012-12-12 11:38:32 +0800

    [BugId]0000776 remove help menu,cause no help action for help menu commit by Tong.Chao

--- src/com/android/settings/SettingsPreferenceFragment.java
+++ src/com/android/settings/SettingsPreferenceFragment.java
@@ -89,15 +89,15 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements
 
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		if (mHelpUrl != null) {
-			Intent helpIntent = new Intent(Intent.ACTION_VIEW,
-					Uri.parse(mHelpUrl));
-			helpIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-					| Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-			MenuItem helpItem = menu.add(0, MENU_HELP, 0, R.string.help_label);
-			helpItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-			helpItem.setIntent(helpIntent);
-		}
+//		if (mHelpUrl != null) {
+//			Intent helpIntent = new Intent(Intent.ACTION_VIEW,
+//					Uri.parse(mHelpUrl));
+//			helpIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+//					| Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+//			MenuItem helpItem = menu.add(0, MENU_HELP, 0, R.string.help_label);
+//			helpItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+//			helpItem.setIntent(helpIntent);
+//		}
 	}
 
 	/*
