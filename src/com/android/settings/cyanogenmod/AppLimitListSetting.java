51e6e710e90a5c4d7f3f9ee618d363c9c6d49781
kunan.zhao <zhaokunan@shendu.com>
2013-01-05 17:40:08 +0800

    Settings : Fix Nullpointer Exception
    Issue : 0000927

--- src/com/android/settings/cyanogenmod/AppLimitListSetting.java
+++ src/com/android/settings/cyanogenmod/AppLimitListSetting.java
@@ -43,72 +43,74 @@ import com.android.settings.Utils;
 
 public class AppLimitListSetting extends SettingsPreferenceFragment {
 
-	private PackageManager pm;
-	private List<PackageInfo> apkInfos;
-	private PreferenceScreen prefset;
-	private List<ResolveInfo> resolveInfos;
+    private PackageManager pm;
+    private List<PackageInfo> apkInfos;
+    private PreferenceScreen prefset;
+    private List<ResolveInfo> resolveInfos;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		addPreferencesFromResource(R.xml.security_settings);
-		
-		final ActionBar bar = getActivity().getActionBar();
-		bar.setIcon(R.drawable.ic_settings_security);
-		pm = getActivity().getPackageManager();
-		prefset = getPreferenceScreen();
-		intiView();
-	}
+        addPreferencesFromResource(R.xml.security_settings);
 
-	@Override
-	public void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-		saveData();
-	}
+        final ActionBar bar = getActivity().getActionBar();
+        bar.setIcon(R.drawable.ic_settings_security);
+        pm = getActivity().getPackageManager();
+        prefset = getPreferenceScreen();
+        intiView();
+    }
 
-	private void intiView() {
-		if (prefset != null) {
-			prefset.removeAll();
-		}
-		String limitList = Settings.System.getString(getActivity()
-				.getApplicationContext().getContentResolver(),
-				Settings.System.APP_LIMIT_LIST);
-		Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-		resolveInfos = pm.queryIntentActivities(mainIntent, 0);
-		for (ResolveInfo info : resolveInfos) {
-			ApplicationInfo applicationInfo = info.activityInfo.applicationInfo;
-			CheckBoxPreference item = new CheckBoxPreference(getActivity());
-			item.setTitle(applicationInfo.loadLabel(pm).toString());
-			item.setIcon(pm.getApplicationIcon(applicationInfo));
-			if(limitList.contains(applicationInfo.packageName)){
-				item.setChecked(true);
-			}else{
-				item.setChecked(false);
-			}
-			prefset.addPreference(item);
-		}
-	}
+    @Override
+    public void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        saveData();
+    }
 
-	private void saveData() {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < prefset.getPreferenceCount(); i++) {
-			CheckBoxPreference item = (CheckBoxPreference) prefset.getPreference(i);
-			boolean isChecked = item.isChecked();
-			if(isChecked){
-				ApplicationInfo applicationInfo = resolveInfos.get(i).activityInfo.applicationInfo;
-				sb.append(applicationInfo.packageName+"|");
-			}
-		}
-		Settings.System.putString(getActivity().getApplicationContext()
-				.getContentResolver(),
-				Settings.System.APP_LIMIT_LIST, sb.toString());
-	}
+    private void intiView() {
+        if (prefset != null) {
+            prefset.removeAll();
+        }
+        String limitList = Settings.System.getString(getActivity()
+                .getApplicationContext().getContentResolver(),
+                Settings.System.APP_LIMIT_LIST);
+        if (limitList != null) {
+            Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+            resolveInfos = pm.queryIntentActivities(mainIntent, 0);
+            for (ResolveInfo info : resolveInfos) {
+                ApplicationInfo applicationInfo = info.activityInfo.applicationInfo;
+                CheckBoxPreference item = new CheckBoxPreference(getActivity());
+                item.setTitle(applicationInfo.loadLabel(pm).toString());
+                item.setIcon(pm.getApplicationIcon(applicationInfo));
+                if (limitList.contains(applicationInfo.packageName)) {
+                    item.setChecked(true);
+                } else {
+                    item.setChecked(false);
+                }
+                prefset.addPreference(item);
+            }
+        }
+    }
 
-	public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
-			Preference preference) {
-		return super.onPreferenceTreeClick(preferenceScreen, preference);
-	}
+    private void saveData() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < prefset.getPreferenceCount(); i++) {
+            CheckBoxPreference item = (CheckBoxPreference) prefset.getPreference(i);
+            boolean isChecked = item.isChecked();
+            if (isChecked) {
+                ApplicationInfo applicationInfo = resolveInfos.get(i).activityInfo.applicationInfo;
+                sb.append(applicationInfo.packageName + "|");
+            }
+        }
+        Settings.System.putString(getActivity().getApplicationContext()
+                .getContentResolver(),
+                Settings.System.APP_LIMIT_LIST, sb.toString());
+    }
+
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
 }
